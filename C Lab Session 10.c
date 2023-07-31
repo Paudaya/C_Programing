a52e2829ecc22b7775b64c6bcc6c3c765d815cb5
Paudaya <135630512+Paudaya@users.noreply.github.com>
2023-07-31 14:57:28 +0530

    C Lab Session 10

---
+++ C Lab Session 10.c
@@ -0,0 +1,20 @@
+# Step 1: Declare and initialize the array
+arr = [5, 10, 3, 8, 1, 7, 9, 4, 2, 6]
+
+# Step 2: Find the minimum value
+min_value = min(arr)
+
+# Step 3: Find the maximum value
+max_value = max(arr)
+
+# Step 4: Find the average value
+average_value = sum(arr) / len(arr)
+
+# Step 5: Reverse the order of values
+reversed_arr = arr[::-1]
+
+# Step 6: Print the results
+print("Minimum value:", min_value)
+print("Maximum value:", max_value)
+print("Average value:", average_value)
+print("Reverse order of values:", reversed_arr)
