f30516a68726a16bcedf476f3b0d4f6e03429d2d
Paudaya <135630512+Paudaya@users.noreply.github.com>
2023-07-31 12:16:59 +0530

    C Lab Session 9

---
+++ C Lab Session 9.3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int base,exp,result=1;
+    printf("Enter the base number: ");
+    scanf("%d",&base);
+    printf("Enter the exponent number: ");
+    scanf("%d",&exp);
+
+    for (int i=0; i<exp; i++)
+    {
+        result*=base;
+
+    }
+    printf("Answer is: %d",result);
+}
+
+
+
+
+
+
+
+
+
